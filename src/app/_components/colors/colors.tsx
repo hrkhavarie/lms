d23d9219b2429faf1de0b99397ad09ba68266605
Hamid Reza Khavarie <hr.khavarie@gmail.com>
2024-03-21 17:17:38 +0330

    tailwind config

--- src/app/_components/colors/colors.tsx
+++ src/app/_components/colors/colors.tsx
@@ -9,16 +9,13 @@ export const Colors: React.FC = () => (
 
     
     <div className="flex flex-wrap justify-center">
-        {
-            
+        {  
             Object.entries(tailwindColors).map(([ name , color])=>(
                 <ColorBox key={name} name={name} color={color} />
                 ))
             }
     </div>
-
 )
-
 
 const ColorBox:React.FC<{name:string , color:string}> =({
     name , 
